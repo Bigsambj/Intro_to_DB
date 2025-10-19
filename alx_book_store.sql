65260199cef5b040dba61354e12fd1da26ae05a2
Samuel Aidoo <samuelaidoo@Samuels-MacBook-Pro-2.local>
2025-10-19 16:01:17 +0000

    add al_book_store database schema file

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -41,17 +41,13 @@ CREATE TABLE IF NOT EXISTS Customers (
 
 -- Orders table
 CREATE TABLE IF NOT EXISTS Orders (
-    order_id BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,
-    customer_id BIGINT UNSIGNED NOT NULL,
+    order_id INT NOT NULL AUTO_INCREMENT,
+    customer_id INT NOT NULL,
     order_date DATE NOT NULL,
     PRIMARY KEY (order_id),
-    INDEX idx_orders_customer_id (customer_id),
-    CONSTRAINT fk_orders_customers
-        FOREIGN KEY (customer_id)
-        REFERENCES Customers (customer_id)
-        ON DELETE RESTRICT
-        ON UPDATE CASCADE
+    FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
 
 -- Order_Details table
 CREATE TABLE IF NOT EXISTS Order_Details (
