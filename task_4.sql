f9ce08bf3c2aed0933f08ce9e1bc555a98ba4564
Samuel Aidoo <samuelaidoo@Samuels-MacBook-Pro-2.local>
2025-10-19 20:30:52 +0000

    Task 4

---
+++ task_4.sql
@@ -0,0 +1,7 @@
+-- File: task_4.sql
+-- Prints the full description of the 'Books' table in the alx_book_store database
+-- without using DESCRIBE or EXPLAIN
+
+USE alx_book_store;
+
+SHOW CREATE TABLE Books;
