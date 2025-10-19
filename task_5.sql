d398ff648c32f5d5fff4be217e888be4af816db4
Samuel Aidoo <samuelaidoo@Samuels-MacBook-Pro-2.local>
2025-10-19 21:58:00 +0000

    Add task_5.sql to insert one customer into Customers table

---
+++ task_5.sql
@@ -0,0 +1,7 @@
+-- File: task_5.sql
+-- Inserts a single row into the Customers table in the alx_book_store database
+
+USE alx_book_store;
+
+INSERT INTO Customers (customer_id, customer_name, email, address)
+VALUES (1, 'Cole Baidoo', 'cbaidoo@sandtech.com', '123 Happiness Ave.');
